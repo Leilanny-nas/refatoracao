d7627725526323529981e9e35591fec3a233b3cd
Leilanny Nascimento <usilva055@gmail.com>
2022-12-08 14:57:53 -0300

    Commit 9

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,4 @@
-public class Movie {
+public class Movie<_title> {
         public static final int  CHILDRENS = 2;
         public static final int  REGULAR = 0;
         public static final int  NEW_RELEASE = 1;
@@ -22,5 +22,10 @@ public class Movie {
         public String getTitle (){
             return _title;
         }
-    }
+        public double getCharge(int daysRented){
+
+            return 0;
+        }
+
+}
 
--- src/Rental.java
+++ src/Rental.java
@@ -2,6 +2,7 @@ public class Rental {
     private Movie _movie;
     private int _daysRented;
     private int frequentRenterPoints;
+    private Rental each;
 
     public Rental(Movie movie, int daysRented) {
         _movie = movie;
@@ -15,13 +16,13 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
-    public int getFrequentRenterPoints(Rental each){
+    public int getFrequentRenterPoints(){
         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                 each.getDaysRented() > 1) frequentRenterPoints ++;
         return 0;
     }
-    public double getCharge(){
 
-        return 0;
+    public double getCharge(){
+        return _movie.getCharge(_daysRented);
     }
 }
